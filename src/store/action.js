d1192bba8ede11bf441266ba70a531acb37ad25a
chenxiaoxing <chenxiaoxing@dianpingdeMacBook-Pro.local>
2018-04-24 18:18:03 +0800

    ls

--- src/store/action.js
+++ src/store/action.js
@@ -4,16 +4,15 @@ import url from '../lib/url.js';
 const categoryId = 1631;
 const isSEO = 'true';
 const source = 3;
-const contentType = 'application/x-www-form-urlencoded'
+const contentType = 'application/x-www-form-urlencoded';
+let formData = new FormData();
+	formData.append('categoryId',categoryId);
+	formData.append('source',source);
+	formData.append('isSEO',isSEO);
 export default {
 	getIndexData({ commit, state }) {
-		let params = [
-			'categoryId=' + categoryId,
-			'styleTagId=' + state.styleTagId,
-			'sceneTagId=' + state.sceneTagId,
-			'source=' + source,
-			'isSEO=' + isSEO
-		].join("&");
+		formData.append('styleTagId',state.styleTagId);
+		formData.append('sceneTagId',state.sceneTagId);
 	return  fetch(`${url}/index`,{
 			method:'post',
 			credentials: 'include',
@@ -21,7 +20,7 @@ export default {
             'Content-Type': contentType,
 			'Cookie': state.cookie
           	},
-			body:params 
+			body:formData 
 		}).then(function(response) {
             	return response.json();
         	}).then(res => {
@@ -32,15 +31,14 @@ export default {
 	},
 	
 	getWaterData({ commit, state }) {
-		let params = [
-			'categoryId=' + categoryId,
-			'styleTagId=' + state.styleTagId,
-			'sceneTagId=' + state.sceneTagId,
-			'source=' + source,
-			'isSEO=' + isSEO,
-			'pagesize='+ state.pageSize,
-			'page='+1
-		].join("&");
+		// let params = [
+		// 	'pagesize='+ state.pageSize,
+		// 	'page='+1
+		// ].join("&");
+		formData.append('styleTagId',state.styleTagId);
+		formData.append('sceneTagId',state.sceneTagId);
+		formData.append('pagesize',state.pageSize);
+		formData.append('page',1);
 	 return fetch(`${url}/indexsearch`,{
 			method:'post',
 			credentials: 'include',
@@ -48,7 +46,7 @@ export default {
             'Content-Type': contentType,
 			 'Cookie': state.cookie
           	},
-			body: params
+			body: formData
 		}).then(function(response) {
             return response.json();
         }).then(res => {
@@ -75,15 +73,10 @@ export default {
 			})
 	},
 	getDetailWater({commit,state}){
-		let params = [
-			'categoryId=' + categoryId,
-			'styleTagId=' + state.detailstyleTagId,
-			'sceneTagId=' + state.detailsceneTagId,
-			'source=' + source,
-			'isSEO=' + isSEO,
-			'pagesize='+ state.pageSize,
-			'page='+1
-		].join("&");
+		formData.append('styleTagId',state.styleTagId);
+		formData.append('sceneTagId',state.sceneTagId);
+		formData.append('pagesize',state.pageSize);
+		formData.append('page',1);
 		return fetch(`${url}/detailmorepic`,{
 				method:'post',
 				credentials: 'include',
@@ -91,7 +84,7 @@ export default {
 				'Content-Type': contentType,
 				'Cookie': state.cookie
 				},
-				body:params
+				body:formData
 			}).then(function(response) {
 				return response.json();
 			}).then(res => {
